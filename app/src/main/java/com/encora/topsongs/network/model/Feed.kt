1af95affcf00d657e8fe4cbcf0248fc71afcbb06
Jitesh <thejitesh@gmail.com>
2022-02-03 14:49:35 +0530

    Introduced new fields

--- app/src/main/java/com/encora/topsongs/network/model/Feed.kt
+++ app/src/main/java/com/encora/topsongs/network/model/Feed.kt
@@ -9,6 +9,12 @@ data class Feed(
     @field:Element(required = false, name = "title")
     var title: String? = "",
 
+    @field:Element(required = false, name = "updated")
+    var updated: String? = "",
+
+    @field:Element(required = false, name = "icon")
+    var icon: String? = "",
+
     @field:ElementList(required = false, name = "entry", entry = "entry", inline = true, empty = true)
     var songList: List<Song> = mutableListOf()
 )
--- app/src/main/java/com/encora/topsongs/network/model/Song.kt
+++ app/src/main/java/com/encora/topsongs/network/model/Song.kt
@@ -9,5 +9,18 @@ data class Song(
     var id: String? = "",
 
     @field:Element(name = "title", required = false)
-    var title: String? = ""
+    var title: String? = "",
+
+    @field:Element(name = "name", required = false)
+    var name: String? = "",
+
+    @field:Element(name = "link", required = false)
+    var link: String? = null
 )
+
+/*@Root(name = "link", strict = false)
+data class Link (
+    @field:Attribute(name = "href", required = false)
+    var href: String = ""
+    *//*   @field:Text var text: String? = null*//*
+)*/
